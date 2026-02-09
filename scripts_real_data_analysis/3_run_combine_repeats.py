24abd606247c9f6247af08450c4979366d56ece5
anphan0828 <ahphan@iastate.edu>
2026-02-09 08:57:23 -0600

    update plots and PCA

--- scripts_real_data_analysis/3_run_combine_repeats.py
+++ scripts_real_data_analysis/3_run_combine_repeats.py
@@ -19,6 +19,7 @@ import random
 from sklearn.metrics import auc
 from matplotlib import pyplot as plt
 import re
+from sklearn.decomposition import PCA
 
 def extract_disease_code_from_dataset(datasetid_file):
     id_to_code = {}
@@ -80,6 +81,10 @@ def combine_repeats(input_files):
             combined_table = table
         else:
             combined_table = pd.merge(combined_table, table, on="term", how="outer", suffixes=('', f'_{repeat}'))
+    if higher_is_better:
+        combined_table['sum_value'] = combined_table[[col for col in combined_table.columns if col.startswith('value')]].sum(axis=1)
+    else:
+        combined_table['sum_value'] = combined_table[[col for col in combined_table.columns if col.startswith('pvalue')]].sum(axis=1)
     return combined_table, higher_is_better
 
 def pheno_pr(combined_table, higher_is_better, phenotype_df):    
@@ -95,7 +100,7 @@ def pheno_pr(combined_table, higher_is_better, phenotype_df):
     prevalence = len(valid_phenotype_terms) / len(combined_table)
     
     # Calculate sum of values across repeats
-    combined_table['sum_value'] = combined_table[[col for col in combined_table.columns if col.startswith('value')]].sum(axis=1)
+    # combined_table['sum_value'] = combined_table[[col for col in combined_table.columns if col.startswith('value')]].sum(axis=1)
     combined_table['true_value'] = combined_table['term'].apply(lambda x: 1 if x in valid_phenotype_terms else 0)    
     
     # Return result across repeats
@@ -203,11 +208,11 @@ with open('main_result.txt', 'a') as f:
         # Resolve ties and get chosen terms
         combined_table, higher_is_better = combine_repeats(input_files)
         if higher_is_better:
-            ties = combined_table.groupby('value').apply(lambda x: len(x)).sort_index(ascending=False).to_dict()
-            table_score = combined_table[['term','value']].rename(columns={"value":"score"})
+            ties = combined_table.groupby('sum_value').apply(lambda x: len(x)).sort_index(ascending=False).to_dict()
+            table_score = combined_table[['term','sum_value']].rename(columns={"sum_value":"score"})
         else:
-            ties = combined_table.groupby('pvalue').apply(lambda x: len(x)).sort_index(ascending=True).to_dict()
-            table_score = combined_table[['term','pvalue']].rename(columns={"pvalue":"score"})
+            ties = combined_table.groupby('sum_value').apply(lambda x: len(x)).sort_index(ascending=True).to_dict()
+            table_score = combined_table[['term','sum_value']].rename(columns={"sum_value":"score"})
         chosen_score = pd.DataFrame(columns=table_score.columns)
         for score, count in ties.items():
             if count >  num - len(chosen_score):
@@ -230,3 +235,43 @@ with open('main_result.txt', 'a') as f:
         res_overlap = calculate_overlap(chosen_score, dict_T)
         print(f'{ID_dataset}_{ID_method}: {res_overlap}')
         f.write(f'{ID_dataset}\t{ID_method}\t{ID_metric_overlap}\t{res_overlap}\n')
+
+# Plotting PCA for repeats
+fig, ax = plt.subplots(ncols=4, nrows=6, figsize=(20, 30))        
+idx=0
+for each in run_list:
+    (dataset, method, metric) = each
+    ID_dataset = str(dataset["ID_dataset"])
+    gene_file = str(dataset["gene_list"])
+    annotation_file = '/work/idoerg/ahphan/benchmark_GSEA/saved_data/' + str(dataset["annotations"])
+    ID_method = str(method["ID_method"]).split("-")[0]  # remove repeat suffix
+    if ID_method != '65':
+        continue
+    idx+=1
+    cancer_id = gene_file.split("/")[-1].split(".")[0].split("_de_limma")[0]
+    input_files = [os.path.join('/work/idoerg/ahphan/benchmark_GSEA/results', x) for x in os.listdir('/work/idoerg/ahphan/benchmark_GSEA/results') if x.startswith(f'{ID_dataset}_{ID_method}-') and x.endswith('_result.tsv')]
+    
+    # Resolve ties and get chosen terms
+    combined_table, higher_is_better = combine_repeats(input_files)
+    pca = PCA(n_components=2)
+    principalComponents = pca.fit_transform(combined_table[[col for col in combined_table.columns if col.startswith('value')]].T)
+    df_pca = pd.DataFrame(data = principalComponents, columns = ['PC1', 'PC2'])
+    
+    ax_idx = (idx-1)//4, (idx-1)%4
+    ax[ax_idx].scatter(df_pca['PC1'], df_pca['PC2'], color='blue', label='All gene sets', alpha=0.7)
+    
+    # Get rows where at least one value* column is >= 0.5
+    filtered_df = combined_table[combined_table[[col for col in combined_table.columns if col.startswith('value')]].ge(0.5).any(axis=1)] 
+    if len(filtered_df) >= 2:
+        pca = PCA(n_components=2)
+        principalComponents = pca.fit_transform(filtered_df[[col for col in filtered_df.columns if col.startswith('value')]].T)
+        filtered_df_pca = pd.DataFrame(data = principalComponents, columns = ['PC1', 'PC2'])
+        
+        ax[ax_idx].scatter(filtered_df_pca['PC1'], filtered_df_pca['PC2'], color='red', label='Gene sets >=0.5', alpha=0.7)
+    ax[ax_idx].set_title(f'{cancer_id}_{ID_method}')
+    ax[ax_idx].set_xlabel('PC1')
+    ax[ax_idx].set_ylabel('PC2')
+
+plt.tight_layout()
+plt.legend()
+plt.savefig('combined_repeats_pca_gamma.png')
